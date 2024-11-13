864b34d5e231849e2989366f4e80a23423d3e09b
陳佳吟 <chen19970221@gmail.com>
2024-11-13 18:03:17 +0800

    修正 active 樣式

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,6 +4,7 @@ import {  defineEmits, reactive } from 'vue'
 const productsList = data.productsList
 const itemSelected = reactive({})
 const emit = defineEmits(['productData'])
+const props = defineProps(['selectedProduct'])
 
 function getProduct(product) {
   itemSelected.name = product.name
@@ -18,7 +19,7 @@ function getProduct(product) {
 <template>
   <div class="p-2">
     <button  v-for="product in productsList" :key="product.name" class="btn form-control p-0" 
-    @click="getProduct(product)" :class="{ 'active': itemSelected.name === product.name}">
+    @click="getProduct(product)" :class="{ 'active': props.selectedProduct.name === product.name}">
       <div class="border border-outline-primary px-3 py-2 text-start">
         <p class="mb-1">{{ product.name }}</p>
         <div class="d-flex justify-content-between mb-0">
